8b07e80e4b8a9a26174ff2833d3747dc093254b0
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-06-14 13:52:20 +0300

    Add light/dark mode

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ taskCard.addEventListener('click', (e) => {
   const classes = e.currentTarget.classList;
   classes.toggle('completed');
 });
+
+const changeTheme = document.querySelector('.header-right');
+changeTheme.addEventListener('click', (e) => {
+  document.querySelector('body').classList.toggle('light');
+});
